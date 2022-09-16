c6fe4d7c085f9bbf227f8046050c6e7f14c14214
Simon <46320904+gamersi@users.noreply.github.com>
2022-09-16 10:48:15 +0200

    fixed bug

--- app.js
+++ app.js
@@ -115,13 +115,13 @@ function Board() {
             s.snakeBody[0] = [s.snakeX, s.snakeY];
         }
 
-        for (let i = s.snakeBody.length - 1; i > 0; i--) {
-            if(snakeBody[i][0] == f.foodX && snakeBody[i][1] == f.foodY) {
-                f.placeFood();
-                this.update();
-                return
-            }
-        }
+        //for (let i = s.snakeBody.length - 1; i > 0; i--) {
+        //     if(snakeBody[i][0] == f.foodX && snakeBody[i][1] == f.foodY) {
+        //         f.placeFood();
+        //         this.update();
+        //         return
+        //     }
+        // }
 
         //food
         this.context.fillStyle = f.foodColor;
